30db1b036a821a371987fe632130849844ef5e4f
Martin Wimpress <code@flexion.org>
2022-08-31 14:25:47 +0100

    Clean up

--- make-few-containers.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for TAG in python jammy distroless; do
-    docker build -f "Dockerfile.${TAG}" -t "app:${TAG}" .
-    docker-slim build --tag "app:${TAG}-slim" "app:${TAG}"
-    echo
-done
